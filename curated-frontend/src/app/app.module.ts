bd3bc7f2b6095d74b2d6228e7f8e064848180015
Kirk Hahn <kirkhahn11@yahoo.com>
2021-10-06 18:29:09 -0500

    starter files for angular components and installed bootstrap

--- curated-frontend/src/app/app.module.ts
+++ curated-frontend/src/app/app.module.ts
@@ -3,14 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './components/nav/nav.component';
+import { UserDetailComponent } from './components/user-detail/user-detail.component';
+import { UsersComponent } from './components/users/users.component';
+import { MovieDetailComponent } from './components/movie-detail/movie-detail.component';
+import { MoviesComponent } from './components/movies/movies.component';
+import { ReviewsComponent } from './components/reviews/reviews.component';
+import { ReviewDetailsComponent } from './components/review-details/review-details.component';
+import { MovieSearchComponent } from './components/movie-search/movie-search.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    UserDetailComponent,
+    UsersComponent,
+    MovieDetailComponent,
+    MoviesComponent,
+    ReviewsComponent,
+    ReviewDetailsComponent,
+    MovieSearchComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
